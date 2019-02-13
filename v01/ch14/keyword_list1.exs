7c2773b8769d64e45d1d4fc111e2b6dfd8d93967
nora inu <modeverv@gmail.com>
2019-02-13 10:23:45 +0900

    2019-02-13 10:23:45

---
+++ v01/ch14/keyword_list1.exs
@@ -0,0 +1,10 @@
+options = [foo: 1, bar: 2]
+IO.inspect options
+IO.inspect options[:foo]
+[{:foo, 1},{:bar, 2}]
+# と
+[foo: 1, bar: 2]
+# は同等
+
+
+
--- v01/ch14/maps1.exs
+++ v01/ch14/maps1.exs
@@ -1,7 +1,25 @@
 ages = %{"Alice"  => 23, "Bob" => 15, "Charlie" => 10}
-ages2 = %{Alice:  23, Bob: 15, Charlie: 10}
+ages2 = %{Alice:  23, age: 15, Charlie: 10}
 
 IO.inspect ages["Bob"]
 IO.inspect ages["David"]
-IO.inspect ages2[:Bob]
+IO.inspect ages2[:Alice]
+IO.inspect ages2.age
+
+ages = %{ ages | "Bob" => 16}
+IO.inspect ages
+
+ages = Map.merge(ages,%{"Tom" => 10})
+IO.inspect ages
+#Map.delete/2
+#Map.drop/2
+
+# Mapの繰り返し
+Enum.each ages, fn({name,value}) ->
+  IO.puts "#{name} => #{value}"
+end
+# for マクロ
+for {name , value} <- ages do
+    IO.puts "#{name} => #{value}"
+end
 
