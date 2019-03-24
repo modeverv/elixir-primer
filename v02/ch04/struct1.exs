dd1b2a6af15f441d7eaab77f2c3bcf246c2a4e6a
nora inu <modeverv@gmail.com>
2019-03-24 13:12:24 +0900

    2019-03-24 13:12:24

---
+++ v02/ch04/struct1.exs
@@ -0,0 +1,11 @@
+m = %{name: "foo", email: "foo@example.com"}
+u = %User{name: "foo", email: "foo@example.com"}
+u2 = %User{:name =>  "foo", :email => "foo@example.com"}
+IO.inspect m
+IO.inspect u
+IO.inspect u2
+IO.inspect m.email
+IO.inspect u.email
+u3 = %User{u | email: "bar@example.com"}
+IO.inspect u3
+
