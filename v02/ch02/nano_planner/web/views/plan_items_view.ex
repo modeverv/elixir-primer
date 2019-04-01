669c362e35ae3bdeef22b881dac331bfe3986133
nora inu <modeverv@gmail.com>
2019-04-01 09:55:37 +0900

    2019-04-01 09:55:37

--- v02/ch02/nano_planner/web/views/plan_items_view.ex
+++ v02/ch02/nano_planner/web/views/plan_items_view.ex
@@ -1,9 +1,19 @@
 defmodule NanoPlanner.PlanItemsView do
   use NanoPlanner.Web, :view
+  alias Timex.Format.DateTime.Formatters.Strftime
 
   def format_duration(item) do
-    "#{item.starts_at} - #{item.ends_at}"
+    [formats_starts_at(item), " - ", formats_ends_at(item)] |> Enum.join("")
   end
+
+  defp formats_starts_at(item) do
+    Strftime.format! item.starts_at, "%Y年%m月%d日 %H:%M"
+  end
+
+  defp formats_ends_at(item) do
+    Strftime.format! item.ends_at, "%Y年%m月%d日 %H:%M"
+  end
+  
   
 end
 
